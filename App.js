eea0485f493e270287a259a2e8e6e5261943dd4d
adhxabre <abeldustin06@gmail.com>
2023-03-17 09:56:23 +0700

    Event

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import EmbedExpressions from "./src/components/EmbedExpressions";
+import Event from "./src/components/Event";
 
 export default function App() {
   return (
     <View style={{ marginTop: 200 }}>
       <StatusBar />
-      <EmbedExpressions />
+      <Event />
     </View>
   );
 }
