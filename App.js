93f91cbd487c7e7d263ae5cda3f50560c9435ece
adhxabre <abeldustin06@gmail.com>
2023-03-17 10:52:40 +0700

    inline styling

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import State from "./src/screens/State";
+import Form from "./src/screens/Form";
 
 export default function App() {
   return (
     <View style={{ marginTop: 200 }}>
       <StatusBar />
-      <State />
+      <Form />
     </View>
   );
 }
