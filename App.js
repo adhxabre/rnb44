0c18c194e7364f9633888dd395f17c3a0207d1ce
adhxabre <abeldustin06@gmail.com>
2023-03-17 10:26:12 +0700

    State

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import Props from "./src/screens/Props";
+import State from "./src/screens/State";
 
 export default function App() {
   return (
     <View style={{ marginTop: 200 }}>
       <StatusBar />
-      <Props />
+      <State />
     </View>
   );
 }
--- src/components/list.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function List(props) {
-  return (
-    <View>
-      <Text>{props.title}</Text>
-    </View>
-  );
-}
