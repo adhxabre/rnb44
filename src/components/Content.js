3a04887ec73bfe066a435c158ae1b626390a9439
adhxabre <abeldustin06@gmail.com>
2023-03-17 09:39:01 +0700

    Components

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Content() {
+  return (
+    <View>
+      <Text>This is Content!</Text>
+      <Text>lorem ipsum</Text>
+    </View>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Header() {
+  return (
+    <View>
+      <Text>This is Header!</Text>
+      <Text>Ini adalah bagian dari header.</Text>
+    </View>
+  );
+}
